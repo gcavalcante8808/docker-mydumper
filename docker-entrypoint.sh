2899d36db9f75aba0a24faf42926039655ae0b2c
gcavalcante88 <gabriel.cavalcante88@gmail.com>
2019-02-16 22:39:31 -0200

    Added MC/S3 support.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+setup_mc() {
+  echo "Setting Up Minio Client"
+  /usr/local/bin/mc --config-dir /tmp config host add default "${S3_ENDPOINT_URL}" "${S3_ACCESS_KEY_ID}" "${S3_SECRET_ACCESS_KEY}"
+}
+
+sync_folder() {
+  echo "Trying to Sync ${BACKUP_FOLDER}"
+  /usr/local/bin/mc --config-dir /tmp/mc.config mirror "${BACKUP_FOLDER}" "default/${S3_BUCKET}/"
+}
+
 if [[ -z "${DB_HOST}" ]]; then
     echo "No DB_HOST Provided. Exiting."
     exit 1
@@ -28,17 +38,21 @@ if [[ -z "${BACKUP_DIR}" ]]; then
 fi
 
 
-if [[ ! -z "${S3_ACCESSKEY}" ]]; then
-    echo "S3 Support recognized, checking secret variable"
-    if [[ ! -z "${S3_SECRET}" ]]; then
-      echo "S3 Secret defined. We save backups from ${BACKUP_DIR} to s3 as well."
-      S3_SUPPORT=1
-    fi
-fi
 
 if [[ ! -d "${BACKUP_DIR}" ]]; then
    echo "Creating backup dir: ${BACKUP_DIR}"
    mkdir -p "${BACKUP_DIR}"
 fi
 
-exec mydumper --compress -h ${DB_HOST} -u ${DB_USER} -p ${DB_PASSWORD} -B ${DB_NAME} -o ${BACKUP_DIR}/${DB_NAME}-$(date +%d-%b-%y)
+BACKUP_FOLDER=${BACKUP_DIR}/${DB_NAME}-$(date +%d-%b-%y)
+
+
+mydumper --compress -h "${DB_HOST}" -u "${DB_USER}" -p "${DB_PASSWORD}" -B "${DB_NAME}" -o "${BACKUP_FOLDER}" 
+
+if [[ ! -z "${S3_ACCESS_KEY_ID}" ]]; then
+    echo "S3 Support recognized, Configuring client and the file will be sent to S3 in after mydumper work."
+    setup_mc
+    sync_folder
+    rm -rf "${BACKUP_FOLDER}"
+    echo "S3 Backup Finished"
+fi
